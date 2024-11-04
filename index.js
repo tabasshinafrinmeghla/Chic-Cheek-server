893a251429cfe4ce4ba14a07d07edf3b05c1ff37
tabasshinafrinmeghla <tabasshinafrinmeghla@gamil.com>
2024-11-04 17:13:33 +0600

    create input section

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const cors = require ('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
-// middleware 
+// middleware
 app.use(cors());
 app.use(express.json());
 
-// 
-// 
+//
+//
 
 // console.log(process.env.DB_User)
 // console.log(process.env.DB_PASS)
@@ -23,7 +23,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -31,8 +31,20 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
+
+    const productCollection = client.db("productDB").collection("product");
+
+    app.post("/product", async (req, res) => {
+      const newProduct = req.body;
+      console.log(newProduct);
+      const result = await productCollection.insertOne(newProduct);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
@@ -40,15 +52,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("glow in your make up");
+});
 
-
-
-
-app.get('/', (req, res)=> {
-    res.send('glow in your make up')
-} )
-
-app.listen(port, () =>
-{
-    console.log(`collection of makeup iteam on port: ${port}`)
-})
+app.listen(port, () => {
+  console.log(`collection of makeup iteam on port: ${port}`);
+});
